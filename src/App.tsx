e2e98dc53db7f82f6ae6631e5e344b3721a48607
harshika53 <rathod6155@gmail.com>
2025-05-27 09:42:09 +0530

    modified routes of the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import ScanPage from './pages/ScanPage';
-// import Layout from './components/Layout';
-// import ReportPage from './pages/ReportPage';
-// import HistoryPage from './pages/HistoryPage';
-// import NotFoundPage from './pages/NotFoundPage';
+import ReportPage from './pages/ReportPage';
+import HistoryPage from './pages/HistoryPage';
+import NotFoundPage from './pages/NotFoundPage';
 
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="text-center mt-10">
-        <h1 className="text-2xl font-bold">WebAble</h1>
-      </div>
-      <Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
         <Route path="scan" element={<ScanPage />} />
-        {/* <Route path="/" element={<Layout />}>
-          <Route path="scan" element={<ScanPage />} />
-          <Route path="report/:id" element={<ReportPage />} />
-          <Route path="history" element={<HistoryPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route> */}
-      </Routes>
-    </BrowserRouter>
+        <Route path="report/:id" element={<ReportPage />} />
+        <Route path="history" element={<HistoryPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
-};
+}
 
 export default App;
