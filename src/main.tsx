90fa842f953c8a7c008db92e9cba9cea53b84826
harshika53 <rathod6155@gmail.com>
2025-07-02 01:25:48 +0530

    minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-// import '../firebase'; // Ensure the correct path to your firebase config file
-
-// If your firebase config is in the same folder as main.tsx, use:
-import './firebase';
-
-// Or, if it's in the src folder:
- // import './firebase';
-
-// Uncomment the correct import above and ensure the firebase.ts file exists at that location.
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
