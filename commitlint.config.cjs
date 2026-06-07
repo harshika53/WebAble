8386ae826bbde06cc0d6d05ea2560645bd89671a
Ashutosh Dash <dashashutosh1999@gmail.com>
2026-06-07 22:49:14 +0530

    feat: add husky and commitlint for improved commit message linting

---
+++ commitlint.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "perf",
+        "ci",
+        "revert",
+      ],
+    ],
+    "type-case": [2, "always", "lowercase"],
+    "subject-case": [2, "always", "lower-case"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 100],
+    "scope-empty": [0],
+    "scope-case": [2, "always", "lower-case"],
+    "body-empty": [0],
+  },
+};
